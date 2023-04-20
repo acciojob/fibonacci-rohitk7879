a753a33144690cd00fce4516f859c2e9d8e877bd
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-20 11:27:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function fibonacci(num) {
 // your code here
+	let n1 = 0;
+	let n2 = 1;
+	var nextValue = 0;
+	for (let index = 3; index < num; index++){
+		nextValue = n1 + n2;
+		n1 = n2;
+		n2 = nextValue;
+	}
+	return nextValue;
 }
 
 module.exports = fibonacci;
