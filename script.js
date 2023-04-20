5354ea988d764a631f4acced087d00389dc14b12
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-20 12:28:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function fibonacci(num) {
 // your code here
 	let n1 = 0;
 	let n2 = 1;
+	if(num <= 2){
+		return num-1;
+	}
 	var nextValue = 0;
 	for (let index = 3; index <= num; index++){
 		nextValue = n1 + n2;
